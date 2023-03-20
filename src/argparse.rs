3db3ba68508e6147beae654e5cb22a6e7f376e9a
Lee Avital <leeavital@gmail.com>
2023-03-20 17:29:54 -0400

    Formatting

--- src/argparse.rs
+++ src/argparse.rs
@@ -7,44 +7,41 @@ pub struct Parsed {
     pub target: String,
 }
 
-impl  Parsed {
-
+impl Parsed {
     pub fn from_args(args_it: &mut Iter<String>) -> Result<Parsed, String> {
-
         let mut dry_run = false;
-        let mut target  = String::new();
+        let mut target = String::new();
         let mut scale_op: Option<Box<dyn FnOnce(i32) -> i32>> = None;
         let mut kubectl_flags = Vec::new();
-
 
         while let Some(arg) = args_it.next() {
             match arg.as_str() {
                 "--dry-run" => {
                     dry_run = true;
-                },
+                }
                 "deployment" => {
                     let v = consume_or_error(args_it, "expected name for deployment")?;
                     target.push_str("deployment/");
                     target.push_str(v.as_str());
-                },
+                }
                 "statefulset" => {
                     let v = consume_or_error(args_it, "expected name for statefulset")?;
                     target.push_str("statefulset/");
                     target.push_str(v.as_str());
-                },
+                }
                 "--replicas" => {
                     let v = consume_or_error(args_it, "expected value for replicas flag")?;
                     match v.parse::<i32>() {
-                        Ok(replicas) =>  {
-                            scale_op = Some(Box::from(move |_: i32| { replicas }));
-                        },
+                        Ok(replicas) => {
+                            scale_op = Some(Box::from(move |_: i32| replicas));
+                        }
                         Err(e) => {
                             return Err(format!("invalid value for --replcias {}", e));
                         }
                     }
-                },
+                }
                 "--help" => {
-                    let s =r###"
+                    let s = r###"
                     kubectl scalex is a wrapper around kubectl scale, with the added functionality of expressing how much you want
                     to scale by instead of specifying a specific number.
 
@@ -58,8 +55,8 @@ impl  Parsed {
 
                     All flags, including --replicas, that work with kubectl-scale will also work with scalex. Use kubectl scale --help for more information.
                     "###;
-                    return  Err(unindent(s));
-                },
+                    return Err(unindent(s));
+                }
                 _ => {
                     if is_single_kube_flag(arg) {
                         kubectl_flags.push(arg.to_string());
@@ -72,18 +69,16 @@ impl  Parsed {
                         target.push_str(arg);
                     } else {
                         scale_op = match parse_op(arg) {
-                            None => None, 
+                            None => None,
                             Some(f) => Some(Box::from(f)),
                         }
                     }
                 }
             }
-
         }
 
-
         if target.len() == 0 {
-            return  Err(String::from("missing target (deployment or statefulset)"));
+            return Err(String::from("missing target (deployment or statefulset)"));
         }
 
         if scale_op.is_none() {
@@ -91,41 +86,32 @@ impl  Parsed {
             std::process::exit(1);
         }
 
-
-        return  Ok(Parsed {
+        return Ok(Parsed {
             kube_args: kubectl_flags,
-            dry_run: dry_run, 
+            dry_run: dry_run,
             scale_op: scale_op.unwrap(),
-            target:target,
+            target: target,
         });
-    
-
     }
-    
 }
 
-
-
-fn consume_or_error(it: &mut Iter<String>, err_msg: &str) -> Result<String, String>
-{
+fn consume_or_error(it: &mut Iter<String>, err_msg: &str) -> Result<String, String> {
     match it.next() {
         Some(v) => Ok(v.to_string()),
-        None => {
-            Err(String::from(err_msg))
-        }
+        None => Err(String::from(err_msg)),
     }
 }
 
-const KUBE_SHORT_FLAGS : [&str; 2] = [
+const KUBE_SHORT_FLAGS: [&str; 2] = [
     "-n", // namespace
     "-c", // context
 ];
 
-const KUBE_LONG_FLAGS : [&str; 25] = [
+const KUBE_LONG_FLAGS: [&str; 25] = [
     "--as",
     "--as-group",
     "--cache-dir",
-    "--certificate-authority" ,
+    "--certificate-authority",
     "--client-certificate",
     "--client-key",
     "--cluster",
@@ -162,33 +148,30 @@ fn is_valued_kube_flag(s: &str) -> bool {
     return KUBE_SHORT_FLAGS.contains(&s) || KUBE_LONG_FLAGS.contains(&s);
 }
 
-
-fn parse_op(s: &str) -> Option<impl FnOnce(i32) -> i32>
-{
+fn parse_op(s: &str) -> Option<impl FnOnce(i32) -> i32> {
     let mut direction = 1;
     let mut absolute_change = 0;
     let mut factor = 1.0;
     let mut unsigned = s;
     if let Some(x) = s.strip_prefix("-") {
         direction = -1;
-        unsigned =  x;
+        unsigned = x;
     } else if let Some(x) = s.strip_prefix("+") {
         unsigned = x;
     }
 
     if let Some(n) = unsigned.strip_suffix("%") {
-        let parsed  : f32 = n.parse().ok()?;
+        let parsed: f32 = n.parse().ok()?;
         factor = (100.0 + (direction as f32 * parsed)) / 100.0;
     } else {
-        let parsed : i32 = unsigned.parse().ok()?;
+        let parsed: i32 = unsigned.parse().ok()?;
         absolute_change = direction * parsed;
     }
 
-    return Some( move |x| {
+    return Some(move |x| {
         return ((x as f32 * factor).floor() + absolute_change as f32) as i32;
     });
 }
-
 
 fn unindent(source: &str) -> String {
     let mut lines = source.lines();
@@ -206,7 +189,6 @@ fn unindent(source: &str) -> String {
     }
     println!("{} prefix", prefix);
 
-
     let mut unindented = String::new();
     unindented.push_str(first_line.strip_prefix(prefix.as_str()).unwrap());
     unindented.push('\n');
@@ -216,13 +198,8 @@ fn unindent(source: &str) -> String {
         unindented.push('\n');
     }
 
-
     unindented
-
-
 }
-
-
 
 #[cfg(test)]
 mod test {
@@ -232,11 +209,9 @@ mod test {
     #[test]
     fn test_parse_op() {
         fn test_expr(i: i32, xform: &str, expected: Option<i32>) {
-            
             let actual = parse_op(xform).map(|scale| scale(i));
             assert_eq!(actual, expected);
         }
-
 
         test_expr(10, "+100%", Some(20));
         test_expr(10, "-100%", Some(0));
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
-use std::{process::Command};
+use std::process::Command;
 
 mod argparse;
-
 
 fn main() {
     let raw_args: Vec<String> = std::env::args().collect();
     let mut args_it = raw_args.iter();
     args_it.next();
 
-
     let args = argparse::Parsed::from_args(&mut args_it).unwrap_or_else(|err| {
         eprintln!("{err}");
         std::process::exit(1);
     });
-
-
 
     let mut get_cmd = Command::new("kubectl");
     get_cmd
@@ -30,31 +26,31 @@ fn main() {
         std::process::exit(1);
     });
 
-    let replicas_for_deployment: i32 = String::from_utf8(output.stdout).unwrap().trim()
-        .parse().unwrap();
+    let replicas_for_deployment: i32 = String::from_utf8(output.stdout)
+        .unwrap()
+        .trim()
+        .parse()
+        .unwrap();
     let target_replicas = (args.scale_op)(replicas_for_deployment).to_string();
 
-
-    let mut scale_args : Vec<&str> = Vec::new();
+    let mut scale_args: Vec<&str> = Vec::new();
     scale_args.extend(args.kube_args.iter().map(|x| x.as_str()));
     scale_args.push("scale");
     scale_args.push(&args.target);
     scale_args.push("--target");
     scale_args.push(&target_replicas);
 
-
     if args.dry_run {
         eprintln!("would scale from {replicas_for_deployment} to {target_replicas}");
-        eprint!("kubectl {}", scale_args.join(" ") );
-    }  else {
+        eprint!("kubectl {}", scale_args.join(" "));
+    } else {
         let mut scale_cmd = Command::new("kubectl");
-        scale_cmd .args(scale_args);
+        scale_cmd.args(scale_args);
 
         scale_cmd.output().unwrap_or_else(|e| {
             eprintln!("scale cmd failed: {:?}: {}", scale_cmd, e);
             std::process::exit(1);
         });
-        
         todo!("non-dry run not implemented");
     }
 }
